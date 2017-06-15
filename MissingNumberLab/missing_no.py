cc9dd27b8aac5fb3bc6bb12ddabe07da35322a93
GloriahMoses <mosesgloriah96@gmail.com>
2017-06-15 14:04:34 +0300

    Missing Number Lab (Programming Logic)

--- MissingNumberLab/missing_no.py
+++ MissingNumberLab/missing_no.py
@@ -1,8 +1,8 @@
 def find_missing(list_1,list_2):
     """This function finds the missing number in one of the given 
-    two lists and returns respective output"""
+    two lists and returns yhe missing numbers"""
     
-    if list_1 ==[] and list_2==[]: #if lists are empty a 0 is returned
+    if list_1 ==[] and list_2==[]: #if lists are empty, 0 is returned
         return 0
     elif (list_1) == (list_2):
         return 0
