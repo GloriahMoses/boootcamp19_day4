c37374854b42b977ae628cf3b3d8e2adc9179613
GloriahMoses <mosesgloriah96@gmail.com>
2017-06-15 17:20:45 +0300

    Binary Search Function

--- BinarySearch/binary_search.py
+++ BinarySearch/binary_search.py
@@ -1,7 +1,7 @@
 class BinarySearch(list):
-    
+
     def __init__(self, a, b):
-        #Initialize list function
+        #constrctor method which Initialize the list 
         list.__init__(self)
         self.list_length = a
         self.step = b
@@ -9,3 +9,22 @@ class BinarySearch(list):
             self.append(integer * b) #appending to the list
         self.length = len(self)    #variable assiged to the length of the list    
     
+    def search(self, element, bottom=0, top=0, count=0):
+    	
+        if not top:
+        	top = self.length - 1
+        if element == self[bottom]:
+            return {'index': bottom, 'count': count}
+        elif element == self[top]:
+            return {'index': top, 'count': count}
+        mid = (bottom + top) // 2
+        if element == self[mid]:
+            return {'index': mid, 'count': count}
+        elif element > self[mid]:
+            bottom = mid + 1
+        elif element < self[mid]:
+            top = mid - 1
+        if bottom >= top:
+            return {'index': -1, 'count': count}
+        count += 1  
+        return self.search(element, bottom, top, count)
